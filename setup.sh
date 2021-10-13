feb75e5fab6bbc989e931abbf395da1cd1ad9050
Vladimir Rogozin <vladimir20040609@gmail.com>
2021-10-13 22:37:09 +0300

    Editing stupid mistakes

--- setup.sh
+++ setup.sh
@@ -9,6 +9,6 @@ sudo pip3 install ipinfo
 
 # Finally.
 clear
-echo -e "\033[92m[*]\033[0m \033[91mRed\033[0mDDoS Tool was installed successfully."
+echo -e "\033[92m[*]\033[0m \033[91mRed\033[0mNetwok Tool was installed successfully."
 echo -e "\n\n\033[93mWarning: To use it you need the following programs installed and being reachable from terminal: 'nmap', 'traceroute', 'python3-pip'.\033[0m"
-echo -e "\n\nTo run it type '\033[1;4mpython3 RDDoS_Tool.py\033[0m'\n\n"
+echo -e "\n\nTo run it type '\033[1;4msudo python3 RedNetwork_Tool.py\033[0m'\n\n"
