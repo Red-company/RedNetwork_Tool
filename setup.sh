38d02ba2a2737ffe2f71f1c81ed1890ae2b3dbfa
Vladimir Rogozin <vladimir20040609@gmail.com>
2021-12-03 17:50:44 +0300

    Mistakes & setup.sh

--- setup.sh
+++ setup.sh
@@ -27,4 +27,5 @@ echo "wappalyzer_key = ''" >> config.py
 clear
 echo -e "\033[92m[*]\033[0m \033[91mRed\033[0mNetwok Tool was installed successfully."
 echo -e "\n\n\033[93mWarning: To use it you need the following programs installed and being reachable from terminal: 'nmap', 'traceroute', 'python3-pip'.\033[0m"
+echo -e "\n\033[93mWarning: To use Red Network Tool you need to edit 'config.py'.\033[0m"
 echo -e "\n\nTo run it type '\033[1;4msudo python3 RedNetwork_Tool.py\033[0m'\n\n"
